7650700e466c8b29d738f43dfe88a4942cc85ce3
Max Theisen <max@dingbat.at>
2015-11-19 08:35:01 +0100

    Added libraries.

--- app/src/main/java/at/dingbat/type/MainActivity.java
+++ app/src/main/java/at/dingbat/type/MainActivity.java
@@ -2,16 +2,24 @@ package at.dingbat.type;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 
     @Override
